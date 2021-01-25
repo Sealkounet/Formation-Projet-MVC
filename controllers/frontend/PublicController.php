4006f5492c79444c644cc50bde277a021f3c4b95
Sealk <55085328+Sealkounet@users.noreply.github.com>
2021-01-25 12:26:10 +0100

    Update PublicController.php

--- controllers/frontend/PublicController.php
+++ controllers/frontend/PublicController.php
@@ -37,7 +37,7 @@ class PublicController
             $prix = (int)trim(htmlspecialchars(addslashes($_POST['prix'])));
             $token = $_POST["stripeToken"];
             $id = (int)trim(htmlspecialchars(addslashes($_POST['id'])));
-            \Stripe\Stripe::setApiKey("sk_test_tUd8SJ5jN1RoynzJ8DMfazVF00dte0eNO6");
+            \Stripe\Stripe::setApiKey("");
             $charge = \Stripe\Charge::create([
                 'amount'=> $prix.'00',
                 'currency'=>'eur',
